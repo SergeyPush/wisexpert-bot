f46ef37dd550854e2efa41de4e4a626708a1fc41
Sergey Pushkovskiy <spushkovskiy@gmail.com>
2022-07-15 22:07:17 +0300

    Updated config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from dotenv import dotenv_values
 from aiogram import Bot, Dispatcher, types
 
 config = dotenv_values('.env')
-API_KEY = config["API_KEY"]
-HOST = config["WEBHOOK_HOST"]
+API_KEY = "5165676503:AAEHXEpgmRPzzSAxYpz_Tysr5v6P985mBD4"
+HOST = "https://git.heroku.com/wisexpert-bot.git"
 
-bot = Bot(config["API_KEY"], parse_mode=types.ParseMode.HTML)
+bot = Bot(API_KEY, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
