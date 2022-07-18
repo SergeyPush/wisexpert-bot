00cdd89f49981a535e6e32c12e2b0f741a341f26
Sergey Pushkovskiy <spushkovskiy@gmail.com>
2022-07-18 21:13:18 +0300

    Updated token

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from dotenv import dotenv_values
 from aiogram import Bot, Dispatcher, types
 
 config = dotenv_values('.env')
-API_KEY = "5165676503:AAEHXEpgmRPzzSAxYpz_Tysr5v6P985mBD4"
+API_KEY = "5165676503:AAECCCPcYxUnTtRnzbhkrSukiwuUx-A3FqQ"
 HOST = "https://orca-app-r6np6.ondigitalocean.app"
 
 bot = Bot(API_KEY, parse_mode=types.ParseMode.HTML)
