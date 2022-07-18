69bfd14e94b3b1c5a77b75e161009a0faa6dcf34
Sergey Pushkovskiy <spushkovskiy@gmail.com>
2022-07-18 20:17:09 +0300

    Fixed host

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher, types
 
 config = dotenv_values('.env')
 API_KEY = "5165676503:AAEHXEpgmRPzzSAxYpz_Tysr5v6P985mBD4"
-HOST = "https://orca-app-r6np6.ondigitalocean.app/"
+HOST = "https://orca-app-r6np6.ondigitalocean.app"
 
 bot = Bot(API_KEY, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
